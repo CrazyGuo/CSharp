0af9fe1a45f6e979b55b88abb0b9804cceef93aa
CrazyGuo <guohuanhuan_cool@126.com>
2015-02-12 11:21:44 +0800

    Clean code

--- Business/Study.BusinessService/ApplicationService.cs
+++ Business/Study.BusinessService/ApplicationService.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Study.ApplicationServices;
 using Study.Entity;
 
@@ -10,38 +6,21 @@ namespace Study.BusinessService
 {
     public class ApplicationService : ServiceStudyBase<Application, ApplicationDto, ApplicationQuery>, IApplicationService
     {
-        /// <summary>
-        /// 初始化应用程序服务
-        /// </summary>
-        /// <param name="unitOfWork">工作单元</param>
-        /// <param name="repository">应用程序仓储</param>
         public ApplicationService( )
         {
             //Repository = repository;
         }
 
-
-        /// <summary>
-        /// 转换为数据传输对象
-        /// </summary>
-        /// <param name="entity">实体</param>
         protected override ApplicationDto ToDto(Application entity)
         {
             return null;// entity.ToDto();
         }
 
-        /// <summary>
-        /// 转换为实体
-        /// </summary>
-        /// <param name="dto">数据传输对象</param>
         protected override Application ToEntity(ApplicationDto dto)
         {
             return null;// dto.ToEntity();
         }
 
-        /// <summary>
-        /// 创建应用程序
-        /// </summary>
         public override ApplicationDto Create()
         {
             ApplicationDto dto = new ApplicationDto { Enabled = true };
--- Business/Study.BusinessService/IApplicationService.cs
+++ Business/Study.BusinessService/IApplicationService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Study.ApplicationServices;
+﻿using Study.ApplicationServices;
 using Study.Entity;
 
 namespace Study.BusinessService
