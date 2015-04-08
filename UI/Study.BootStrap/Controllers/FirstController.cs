ed79bab531f98c82f622009b68e8773f67ef6b47
CrazyGuo <guohuanhuan_cool@126.com>
2015-04-08 11:38:50 +0800

    添加bootstrap 与d3
    Signed-off-by: CrazyGuo <guohuanhuan_cool@126.com>

---
+++ UI/Study.BootStrap/Controllers/FirstController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Study.MongoDB.Core;
+using Newtonsoft.Json;
+using Study.BootStrap.Models;
+
+namespace Study.BootStrap.Controllers
+{
+    public class FirstController : Controller
+    {
+        private MongoDbRemoteRepository<OpcModel> mongoDbRemoteRepository;
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string TagData(string tagNo)
+        {
+            this.mongoDbRemoteRepository = new MongoDbRemoteRepository<OpcModel>();
+            mongoDbRemoteRepository.Connect();
+            var opcModels = mongoDbRemoteRepository.List(i => i.TagNo == tagNo).OrderBy(i => i.Time).ToList();
+            string sd = "[";
+            int k = 1;
+            foreach (var modle in opcModels)
+            {
+                sd = sd + "{" + "\"Time\":\"" + modle.Id.Created.ToString("yyyy-MM-dd hh:mm:ss") + "\"," + "\"Value\"" + ":" + modle.Value + "},";
+                k++;
+                if(k>=100)
+                {
+                    break;
+                }
+            }
+            sd = sd.Substring(0, sd.Length - 1) + "]";
+            string content = JsonConvert.SerializeObject(opcModels);
+            mongoDbRemoteRepository.CloseConnect();
+            return sd;
+        }
+    }
+}
---
+++ UI/Study.BootStrap/Models/OpcModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB;
+
+namespace Study.BootStrap.Models
+{
+    public class OpcModel
+    {
+        public Oid Id { get; set; }
+        public string TagNo { get; set; }
+        public double Value { get; set; }
+        public string Time { get; set; }
+    }
+}
