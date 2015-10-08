4847a7ec7304a100c1b776a10ffa4fb192d10d04
CrazyGuo <guohuanhuan_cool@126.com>
2015-10-08 09:39:07 +0800

    对导航菜单进行分类,完善树形应用菜单
    Signed-off-by: CrazyGuo <guohuanhuan_cool@126.com>

--- UI/Study.Webs.EayUI/Controllers/HomeController.cs
+++ UI/Study.Webs.EayUI/Controllers/HomeController.cs
@@ -13,11 +13,18 @@ namespace Study.Webs.EayUI.Controllers
         public ActionResult GetTree()
         {
             List<MenuItem> menus = new List<MenuItem>();
-            menus.Add(new MenuItem { id = 1, text = "应用程序管理", attributes = new Attributes { url = "systems/application" } });
+
+            //模块一菜单
+            List<MenuItem> fuctionOneChilden = new List<MenuItem>();
+            fuctionOneChilden.Add(new MenuItem { id = 14, text = "SportRecord", attributes = new Attributes { url = "systems/SportRecord" } });
+            fuctionOneChilden.Add(new MenuItem { id = 5, text = "SportKind", attributes = new Attributes { url = "systems/SportKind" } });
+            MenuItem fuctionOne = new MenuItem { id = 1, text = "目录菜单",  children = fuctionOneChilden };
+
+            menus.Add(fuctionOne);
+            //模块二菜单
+            menus.Add( new MenuItem { id = 1, text = "应用程序管理", attributes = new Attributes { url = "systems/application" }});
             menus.Add(new MenuItem { id = 2, text = "MoneyOut", attributes = new Attributes { url = "systems/moneyout" } });
-            menus.Add(new MenuItem { id = 3, text = "D3Report", attributes = new Attributes { url = "D3Report/FirstReport" } });
-            menus.Add(new MenuItem { id = 4, text = "SportRecord", attributes = new Attributes { url = "systems/SportRecord" } });
-            menus.Add(new MenuItem { id = 5, text = "SportKind", attributes = new Attributes { url = "systems/SportKind" } });
+            menus.Add(new MenuItem { id = 3, text = "D3Report", attributes = new Attributes { url = "D3Report/FirstReport" } });            
             menus.Add(new MenuItem { id = 5, text = "MySql", attributes = new Attributes { url = "OtherDB/User" } });
 
             return Json(menus,JsonRequestBehavior.AllowGet);
@@ -29,7 +36,8 @@ namespace Study.Webs.EayUI.Controllers
         /*这些字段都是小写标志 easyUI tree的要求*/
         public int id { get; set; }
         public string text { get; set; }
-        public Attributes attributes { get; set; }  
+        public Attributes attributes { get; set; }
+        public List<MenuItem> children{get;set;}
     }
     public class Attributes
     {
